9cebb8eea94aa3b7fb0cfe5330c2c46d800309b3
Michael <german.makarkin@gmail.com>
2024-09-29 00:22:24 +0300

    fix tabnine

---
+++ .config/nvim/lua/plugins/tabnine.lua
@@ -0,0 +1,16 @@
+return {
+  "tzachar/cmp-tabnine",
+  build = {
+    LazyVim.is_win() and "pwsh -noni .\\install.ps1" or "./install.sh",
+    ":CmpTabnineHub",
+  },
+  dependencies = "hrsh7th/nvim-cmp",
+  opts = {
+    max_lines = 1000,
+    max_num_results = 3,
+    sort = true,
+  },
+  config = function(_, opts)
+    require("cmp_tabnine.config"):setup(opts)
+  end,
+}
